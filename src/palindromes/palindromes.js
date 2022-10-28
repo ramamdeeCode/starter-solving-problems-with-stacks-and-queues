d02b2209029b7554acd59942e17bef78594fc90c
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2022-10-28 16:18:29 -0400

    parentheses solutions

--- src/palindromes/palindromes.js
+++ src/palindromes/palindromes.js
@@ -1,19 +1,53 @@
+// A palindrome is a word, phrase, or number that is spelled the same
+// forward and backward. For example, “dad” is a palindrome; “A man,
+// a plan, a canal: Panama” is a palindrome if you take out the spaces
+// and ignore the punctuation; and 1,001 is a numeric palindrome. We
+// can use a stack to determine whether or not a given string is a palindrome.
+
+// Write an algorithm that uses a stack to determine whether a given
+// input is palindrome or not. Use the following template as a starting
+// point.
+
+/*
+function is_palindrome(s) {
+    s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    // Your code goes here
+}
+// True, true, true, false
+console.log(is_palindrome("dad"));
+console.log(is_palindrome("A man, a plan, a canal: Panama"));
+console.log(is_palindrome("1001"));
+console.log(is_palindrome("Tauhida"));
+*/
 const Stack = require("../lib/stack");
 
-const isPalindrome = (sentence) => {
-  sentence = sentence.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
-  // Your solution here
+const isPalindrome = (string) => {
+  // Lowercase and remove special characters from string
+  string = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+
+  // Create a new stack
   let stack = new Stack();
 
-  //Populate our stack with our string character;
-  for (let i = 0; i < sentence.length; i++) {
-    stack.push(sentence[i]);
+  // Populate our stack with our string characters
+  for (let i = 0; i < string.length; i++) {
+    stack.push(string[i]);
   }
+
+  // Declare a variable to store the reverse order
   let reverseString = "";
+
+  // While the top isn't null, pop the values off the stack
   while (stack.top !== null) {
     reverseString += stack.pop();
   }
-  return sentence === reverseString;
-};
 
+  // Validate if both strings are the same
+
+  return string === reverseString;
+};
 module.exports = isPalindrome;
+
+// console.log(is_palindrome("dad"));
+// console.log(is_palindrome("A man, a plan, a canal: Panama"));
+// console.log(is_palindrome("1001"));
+// console.log(is_palindrome("Tauhida"));
--- src/parentheses/parentheses.js
+++ src/parentheses/parentheses.js
@@ -1,5 +1,25 @@
 const Stack = require("../lib/stack");
 
-const match = (expression) => {};
+const match = (expression) => {
+  let stack = new Stack();
+  for (let i = 0; i < expression.length; i++) {
+    if (expression[i] === "(") {
+      stack.push("(");
+    } else {
+      if (expression[i] === ")") {
+        if (stack.top !== null) {
+          stack.pop("(");
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+  if (stack.top === null) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 module.exports = match;
