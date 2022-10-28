f6f7bd879e78f34665d1b79fd1e90ee8e66ab74e
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2022-10-28 15:44:24 -0400

    solution palindromes

--- src/palindromes/palindromes.js
+++ src/palindromes/palindromes.js
@@ -3,6 +3,17 @@ const Stack = require("../lib/stack");
 const isPalindrome = (sentence) => {
   sentence = sentence.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
   // Your solution here
+  let stack = new Stack();
+
+  //Populate our stack with our string character;
+  for (let i = 0; i < sentence.length; i++) {
+    stack.push(sentence[i]);
+  }
+  let reverseString = "";
+  while (stack.top !== null) {
+    reverseString += stack.pop();
+  }
+  return sentence === reverseString;
 };
 
 module.exports = isPalindrome;
